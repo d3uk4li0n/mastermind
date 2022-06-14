5ff42b9bc81f0426a948a7a9c4c798480fc13fdb
d3uk4li0n <99892625+d3uk4li0n@users.noreply.github.com>
2022-06-14 13:14:43 +0000

    Update mastermind.h

--- mastermind.h
+++ mastermind.h
@@ -5,6 +5,7 @@ struct cmdline_args{
 
 int check_code(char *);
 int verify_code(char *, char *);
+char *random_code_gen(void)
 void play(char *, int);
 int my_strcmp(char *, char *);
 int my_strlen(const char *);
